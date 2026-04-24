7e8f14cca78c862211cd30fc332a9366483b587e
erinnoviatriyutta <erinnoviatriyutta@gmail.com>
2026-04-24 20:52:34 +0700

    erin05032026

---
+++ RelasiDemo.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package erin05032026;
+
+/**
+ *
+ * @author user
+ */
+public class RelasiDemo {
+     public static void main(String[] args){
+         //a few numbers
+         int  i = 37;
+         int  j = 42;
+         int k = 42;
+          System.out.println("Variable Values...");
+          System.out.println(" i = " + i);
+          System.out.println(" j = " + j);
+          System.out.println(" k = " + k);
+          
+          //lebih besar dari
+          System.out.println("Greater than...");
+          System.out.println(" i > j = " + (i > j)); //false
+          System.out.println(" j > i = " + (j > i)); //true
+          System.out.println(" k > j = " + (k > j)); //false
+          
+          //lebih besar atau samadengan
+          System.out.println("Greater than or equal to...");
+          System.out.println(" i >= j = " + (i >= j)); //false
+          System.out.println(" j >= i = " + (j >= i)); //true
+          System.out.println(" k >= j = " + (k >= j)); //true
+          
+          //sama dengan
+          System.out.println("Equal to...");
+          System.out.println(" i ==> j = " + (i == j)); //false
+          System.out.println(" k == j = " + (k == j)); //true
+          
+          //tidak sama dengan
+          System.out.println("Not equal to...");
+          
+          
+     }
+}
---
+++ VariableSamples.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package erin05032026;
+
+/**
+ *
+ * @author user
+ */
+public class VariableSamples {
+     public static void main ( String[] args ){
+        //deklarasi a tipe data dengan nama variable
+        // result dan tipe data boolean 
+        boolean result;
+        
+        //deklarasi tipe data dengan nama variable
+        // option dan tipe data char 
+        char option;
+        option = 'C';  //menandai 'c' sebagai option
+        
+        //deklarasi tipe data dengan nama variable
+        //grade, double tipe data dan telah di inisalisasi
+        //to 0.0
+        double grade = 0.0; 
+        //System.out.println(result);
+        System.out.println(option);
+        System.out.println(grade);
+    }
+}
