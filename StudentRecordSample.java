eaa44894794a3e99ff764c4789a9aa2c8c689165
erinnoviatriyutta <erinnoviatriyutta@gmail.com>
2026-07-06 19:10:17 +0700

    erin09042026

--- StudentRecordSample.java
+++ StudentRecordSample.java
@@ -2,57 +2,73 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package erin07042026;
+package erin09042026;
 
 /**
  *
  * @author user
  */
 public class StudentRecordSample {
-    public static void  main (String[] args){
-        StudentRecord Ali = new StudentRecord();
-        Ali.setName("Ali");
-        Ali.setaddress("Padang");
-        Ali.setage(20);
-        Ali.setmathGrade(70);
-        Ali.setenglishGrade(80);
-        Ali.setscienceGrade(90);
+    public static void main(String[]args){
+        StudentRecord AniRecord = new StudentRecord();  
+        StudentRecord AliRecord = new StudentRecord(); 
+        StudentRecord DeniRecord = new StudentRecord("Deni","Batusangkar", 10);
+        DeniRecord.jenisKelamin = "L";
+        DeniRecord.print("");
         
-        StudentRecord Ani = new StudentRecord();
-        Ani.setName("Ani");
-        Ani.setaddress("Jakarta");
-        Ani.setage(18);
-        Ani.setmathGrade(80);
-        Ani.setenglishGrade(90);
-        Ani.setscienceGrade(75);
+        System.out.println("");
         
-        System.out.println("Data Ali Safiansyah");
-        System.out.println("Nama                :    "+Ali.getname());
-        System.out.println("Alamat              :    "+Ali.getaddress());
-        System.out.println("Umur                :    "+Ali.getage());
-        System.out.println("Matematika          :    "+Ali.getmathGrade());
-        System.out.println("Inggris             :    "+Ali.getenglishGrade());
-        System.out.println("Ilmu pengetahuan    :    "+Ali.getscienceGrade());
-        System.out.println("Rata-rata           :    "+Ali.getaverage());
+        AliRecord.setName("Ali");
+        AliRecord.setAddress("Padang");
+        AliRecord.setAge(20);
+        AliRecord.setMathGrade(80);
+        AliRecord.setEnglishGrade(70);
+        AliRecord.setScienceGrade(90);
+        
+        AniRecord.setName("Ani");
+        AniRecord.setAddress("Jakarta");
+        AniRecord.setAge(18);
+        AniRecord.setMathGrade(80);
+        AniRecord.setEnglishGrade(90);
+        AniRecord.setScienceGrade(75);
+        
+        System.out.println("Data 1");
+        System.out.println("Nama                :    "+AliRecord.getName());
+        System.out.println("Alamat              :    "+AliRecord.getAddress());
+        System.out.println("Umur                :    "+AliRecord.getAge());
+        System.out.println("Matematika          :    "+AliRecord.getMathGrade());
+        System.out.println("Inggris             :    "+AliRecord.getEnglishGrade());
+        System.out.println("Ilmu pengetahuan    :    "+AliRecord.getScienceGrade());
+        System.out.println("Rata-rata           :    "+AliRecord.getAverage());
         
         System.out.println();
         
-        System.out.println("Data Anisa Putri");
-        System.out.println("Nama                :    "+Ani.getname());
-        System.out.println("Alamat              :    "+Ani.getaddress());
-        System.out.println("Umur                :    "+Ani.getage());
-        System.out.println("Matematika          :    "+Ani.getmathGrade());
-        System.out.println("Inggris             :    "+Ani.getenglishGrade());
-        System.out.println("Ilmu pengetahuan    :    "+Ani.getscienceGrade());
-        System.out.println("Rata-rata           :    "+Ani.getaverage());
+        System.out.println("Data 2");
+        System.out.println("Nama                :    "+AniRecord.getName());
+        System.out.println("Alamat              :    "+AniRecord.getAddress());
+        System.out.println("Umur                :    "+AniRecord.getAge());
+        System.out.println("Matematika          :    "+AniRecord.getMathGrade());
+        System.out.println("Inggris             :    "+AniRecord.getEnglishGrade());
+        System.out.println("Ilmu pengetahuan    :    "+AniRecord.getScienceGrade());
+        System.out.println("Rata-rata           :    "+AniRecord.getAverage());
         
+        System.out.println();
         
+        AniRecord.setName("Ane");
+        AniRecord.setAddress("Pesel");
+        AniRecord.setAge(15);
+        AniRecord.setMathGrade(80);
+        AniRecord.setEnglishGrade(70);
+        AniRecord.setScienceGrade(90);
+        AniRecord.print("");
         
+        System.out.println("");
         
+        System.out.println("jumlah siswa ="+ StudentRecord.getStudentCount());
+    }
+}
        
         
         
-        
-    }   
-    
-}
+  
+
